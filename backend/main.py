48c52c7570a185554b88ff3b5b9633ba0b4a4f3f
pancholi.deep11@gmail.com <pancholi.deep11@gmail.com>
2025-05-31 09:35:20 +0530

    deploy

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,18 @@
+import os
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
 from routes.auth_routes import router as auth_router
 from routes.email_routes import router as email_router
 from core.config import REDIRECT_URL
 
 app = FastAPI()
+
+app.mount("/", StaticFiles(directory="frontend/build", html=True), name="static")
+@app.get("/{full_path:path}")
+async def catch_all(full_path: str):
+    return FileResponse("frontend/build/index.html")
 
 app.add_middleware(
     CORSMiddleware,
