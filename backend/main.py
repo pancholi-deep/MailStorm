3d72729bb874adc4849e1744e23e6a324ea8a322
pancholi.deep11@gmail.com <pancholi.deep11@gmail.com>
2025-05-31 10:32:00 +0530

    origins correction

--- backend/main.py
+++ backend/main.py
@@ -2,12 +2,18 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from backend.routes.auth_routes import router as auth_router
 from backend.routes.email_routes import router as email_router
+from backend.core.config import REDIRECT_URL
 
 app = FastAPI()
 
+origins = [
+    REDIRECT_URL,  # frontend dev server
+    "https://mass-email-frontend.onrender.com",  # add this for production
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://mass-email-frontend.onrender.com"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
