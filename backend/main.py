7838f70239bb0ff2073d7ccdc5027bfc06e32719
pancholi.deep11@gmail.com <pancholi.deep11@gmail.com>
2025-06-10 11:32:10 +0530

    Modified backend paths

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from routes.auth_routes import router as auth_router
-from routes.email_routes import router as email_router
-from core.config import REDIRECT_URL
+from backend.routes.auth_routes import router as auth_router
+from backend.routes.email_routes import router as email_router
+from backend.core.config import REDIRECT_URL
 
 app = FastAPI()
 
